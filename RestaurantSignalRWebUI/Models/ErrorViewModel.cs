dab8356d737ef0bf7d015f6e83a991d943d1bd36
Gokhan <engokhangok@gmail.com>
2023-12-21 12:18:35 +0300

    Add project files.

---
+++ RestaurantSignalRWebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestaurantSignalRWebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
