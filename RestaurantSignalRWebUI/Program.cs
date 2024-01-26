38c125ebcde4630b94fd11af7202588a29dd0780
Gokhan <engokhangok@gmail.com>
2024-01-27 00:06:43 +0300

    product ,social media ,feature, testimonial ,discount, contact,category about crud operations and theme were uploaded and revised by admin

--- RestaurantSignalRWebUI/Program.cs
+++ RestaurantSignalRWebUI/Program.cs
@@ -1,9 +1,14 @@
+using RestaurantSignalR.DataAccess.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
